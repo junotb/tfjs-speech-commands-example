11c0932a243e95c36159ee8be43618bcf58484d3
sj202117 <sj202117@gmail.com>
2024-01-26 01:58:20 +0900

    edit service

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ const tf = require('@tensorflow/tfjs');
 const speechCommands = require('@tensorflow-models/speech-commands');
 
 export default function Home() {
+  const recognizerRef = useRef(null);
   const labelRef = useRef(null);
   const [onListen, setOnListen] = useState(false);
   const [words, setWords] = useState(null);
@@ -25,8 +26,12 @@ export default function Home() {
   };
 
   const startHandler = async () => {
-    const recognizer = await createModel();
-    const classLabels = recognizer.wordLabels(); // get class labels
+    if (!recognizerRef.current) {
+      console.log('createModel');
+      recognizerRef.current = await createModel();
+    }
+    
+    const classLabels = recognizerRef.current.wordLabels(); // get class labels
     for (let i = 0; i < classLabels.length; i++) {
       labelRef.current.appendChild(document.createElement("div"));
     }
@@ -34,7 +39,7 @@ export default function Home() {
     // listen() takes two arguments:
     // 1. A callback function that is invoked anytime a word is recognized.
     // 2. A configuration object with adjustable fields
-    recognizer.listen(result => {
+    recognizerRef.current.listen(result => {
       const scores = result.scores; // probability of prediction for each class
       // render the probability scores per class
       for (let i = 0; i < classLabels.length; i++) {
@@ -48,33 +53,34 @@ export default function Home() {
       overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
     });
 
-    // Stop the recognition in 5 seconds.
-    // setTimeout(() => recognizer.stopListening(), 5000);
+    setOnListen(true);
   };
 
   const stopHandler = async () => {
+    recognizerRef.current.stopListening();
 
+    setOnListen(false);
   };
 
   return (
-    <div className='flex flex-col justify-center items-center w-full h-screen'>
+    <div className='flex flex-col items-center justify-center w-full h-screen'>
       <div>
         <button
           type='button'
           onClick={startHandler}
           disabled={onListen}
-          className='border border-black dark:border-white m-1 p-3 rounded-md bg-white dark:bg-black text-xl text-black dark:text-white'
+          className='p-3 m-1 text-xl text-black bg-white border border-black rounded-md dark:border-white dark:bg-black dark:text-white'
         >Start</button>
         <button
           type='button'
           onClick={stopHandler}
           disabled={!onListen}
-          className='border border-black dark:border-white m-1 p-3 rounded-md bg-white dark:bg-black text-xl text-black dark:text-white'
+          className='p-3 m-1 text-xl text-black bg-white border border-black rounded-md dark:border-white dark:bg-black dark:text-white'
         >Stop</button>
       </div>
       <div
         ref={labelRef}
-        className='m-1 p-3'></div>
+        className='p-3 m-1'></div>
     </div>
   );
 }
